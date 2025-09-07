c3b2f3f460876629532d81fd554c5149bc9d01af
Samarthya Singh <121476592+samarthya-singh@users.noreply.github.com>
2025-09-07 18:07:17 +0530

    Add files via upload

---
+++ SQL/tabel_creattion.sql
@@ -0,0 +1,52 @@
+create database contact_management ;
+use contact_management ;
+
+-- Create Company Table
+CREATE TABLE Company (
+    CompanyID INT PRIMARY KEY,
+    CompanyName VARCHAR(45),
+    Street VARCHAR(45),
+    City VARCHAR(45),
+    State VARCHAR(2),
+    Zip VARCHAR(10)
+);
+-- Create Contact Table
+CREATE TABLE Contact (
+    ContactID INT PRIMARY KEY,
+    CompanyID INT,
+    FirstName VARCHAR(45),
+    LastName VARCHAR(45),
+    Street VARCHAR(45),
+    City VARCHAR(45),
+    State VARCHAR(2),
+    Zip VARCHAR(10),
+    IsMain BOOLEAN,
+    Email VARCHAR(45),
+    Phone VARCHAR(12),
+    FOREIGN KEY (CompanyID) REFERENCES Company(CompanyID)
+);
+
+-- Create Employee Table
+CREATE TABLE Employee (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(45),
+    LastName VARCHAR(45),
+    Salary DECIMAL(10,2),
+    HireDate DATE,
+    JobTitle VARCHAR(25),
+    Email VARCHAR(45),
+    Phone VARCHAR(12)
+);
+
+-- Create ContactEmployee Table
+CREATE TABLE ContactEmployee (
+    ContactEmployeeID INT PRIMARY KEY,
+    ContactID INT,
+    EmployeeID INT,
+    ContactDate DATE,
+    Description VARCHAR(100),
+    FOREIGN KEY (ContactID) REFERENCES Contact(ContactID),
+    FOREIGN KEY (EmployeeID) REFERENCES Employee(EmployeeID)
+);
+
+
